5fb7542bd29b1d17adc844cb7c47117fcc525327
Michael S <mihai.sanfran@gmail.com>
2019-03-03 21:29:08 -0700

    Shortcut to wire up action creator inside of component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
+
     render(){
         return (
             <div>Posts Index Page</div>
@@ -8,4 +14,6 @@ class PostsIndex extends Component {
     }
 }
 
-export default PostsIndex;
+// Shortcut to wire up action creator inside of component; this.props.fetchPosts
+// null for mapStateToProps; pass in action creator itself inside of an object
+export default connect(null, { fetchPosts })(PostsIndex);
