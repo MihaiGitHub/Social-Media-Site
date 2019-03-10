af7b8d2539ff70dee8cdb8d5ee964ceb2ae72ffa
Michael S <mihai.sanfran@gmail.com>
2019-03-09 19:47:47 -0700

    Wire redux form into application

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  // Wire reducer from redux form into combineReducers call
+  // All forms in all components will assume that the form reducer is being applied to the form piece of state
+  form: formReducer
 });
 
 export default rootReducer;
