6413ce4754072a1c0533984dbac05472e0af9fa8
Michael S <mihai.sanfran@gmail.com>
2019-03-16 10:26:29 -0700

    Use mapStateToProps to return single post into component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+    componentDidMount(){
+        // Get post id from URL
+        // Prop provided by react router
+        const { id } = this.props.match.params;
+
+        // Call action creator to fetch post using the id
+        this.props.fetchPost(id);
+    }
+
     render(){
         return (
             <div>
@@ -10,4 +21,11 @@ class PostsShow extends Component {
     }
 }
 
-export default PostsShow;
+// Destructure to get posts piece of state
+// ownProps - props object that is going to this component
+function mapStateToProps({ posts }, ownProps){
+    // return single post 
+    return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
